35d9f4b6e3787fa3ecbcfbd41400d1126ba603ac
Wolfram Woerner <wolfram.woerner@outlook.de>
2022-12-30 17:05:49 +0100

    init

---
+++ premake5.lua
@@ -0,0 +1,106 @@
+workspace "Raytracy"
+	architecture "x86_64"
+	startproject "Sandbox"
+
+	configurations {
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+	flags {
+		"MultiProcessorCompile"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+-- Include directories relative to root folder (solution directory)
+IncludeDir = {}
+
+project "Raytracy"
+	location "Raytracy"
+	kind "StaticLib"
+	language "C++"
+	cppdialect "C++20"
+	staticruntime "off"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files {
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	defines {
+		"_CRT_SECURE_NO_WARNINGS"
+	}
+
+	includedirs {
+		"%{prj.name}/src"
+	}
+
+
+	filter "system:windows"
+		systemversion "latest"
+
+		defines {
+		}
+
+	filter "configurations:Debug"
+		defines "RT_DEBUG"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		defines "RT_RELEASE"
+		runtime "Release"
+		optimize "on"
+
+	filter "configurations:Dist"
+		defines "RT_DIST"
+		runtime "Release"
+		optimize "on"
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+	cppdialect "C++20"
+	staticruntime "off"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files {
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs {
+		"Raytracy/src"
+	}
+
+	links {
+		"Raytracy"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+
+	filter "configurations:Debug"
+		defines "RT_DEBUG"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		defines "RT_RELEASE"
+		runtime "Release"
+		optimize "on"
+
+	filter "configurations:Dist"
+		defines "RT_DIST"
+		runtime "Release"
+		optimize "on"
+
+
