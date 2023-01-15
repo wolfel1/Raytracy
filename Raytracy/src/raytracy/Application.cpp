1af1dfecf9a132ac04943d7b2099a90fa6621bf7
Wolfram Woerner <wolfram.woerner@outlook.de>
2023-01-15 15:41:38 +0100

    Try multithreading

--- Raytracy/src/raytracy/Application.cpp
+++ Raytracy/src/raytracy/Application.cpp
@@ -26,41 +26,6 @@ namespace raytracy {
 	void Application::Run() {
 		RTY_PROFILE_FUNCTION();
 
-		const auto aspect_ratio = 16.0f / 9.0f;
-		ImageData image_data{};
-		HitableCollection scene;
-
-		image_data.width = 480;
-		image_data.height = static_cast<int>(image_data.width / aspect_ratio);
-		image_data.samples_per_pixel = 100;
-		image_data.max_depth = 50;
-
-		auto material_ground =
-			make_shared<LambertianDiffuse>(Color3(0.8f, 0.8f, 0.0f));
-		auto material_center =
-			make_shared<LambertianDiffuse>(Color3(0.7f, 0.3f, 0.3f));
-		auto material_left = make_shared<Dielectric>(1.5f);
-		auto material_right = make_shared<Metal>(Color3(0.8f, 0.6f, 0.2f), 0.0f);
-
-		scene.Add(make_shared<Sphere>(Point3(0.0f, -100.5f, -1.0f), 100.0f,
-									  material_ground));
-		scene.Add(
-			make_shared<Sphere>(Point3(0.0f, 0.0f, -1.0f), 0.5f, material_center));
-		scene.Add(
-			make_shared<Sphere>(Point3(-1.0f, 0.0f, -1.0f), 0.5f, material_left));
-		scene.Add(
-			make_shared<Sphere>(Point3(-1.0f, 0.0f, -1.0f), -0.4f, material_left));
-		scene.Add(
-			make_shared<Sphere>(Point3(1.0f, 0.0f, -1.0f), 0.5f, material_right));
-
-		Point3 look_from(-2, 2, 1);
-		Point3 look_at(0, 0, -1);
-		glm::vec3 up(0, 1, 0);
-
-		Camera camera(look_from, look_at, up, 20.0f, aspect_ratio);
-
-		Raytracer::Submit(scene, camera, image_data);
-
 		std::cin.get();
 		/*while (running) {
 
--- Raytracy/src/raytracy/raytracer/Raytracer.cpp
+++ Raytracy/src/raytracy/raytracer/Raytracer.cpp
@@ -1,19 +1,47 @@
 #include "raytracypch.h"
 #include "Raytracer.h"
 
+#include <execution>
+
 namespace raytracy {
-	void Raytracer::Init() {
+	void Raytracer::Init() {}
 
-	}
-
-	void Raytracer::Submit(const HitableCollection& objects, const Camera& camera, const ImageData& data) {
-		RTY_PROFILE_FUNCTION(); 
+	void Raytracer::Submit(const HitableCollection& objects, const Camera& camera,
+		const ImageData& data) {
+		RTY_PROFILE_FUNCTION();
 		InstrumentationTimer t("Raytracing", true);
 
 		std::ofstream output_stream("./image.ppm", std::ios::out | std::ios::binary);
 		output_stream << "P3\n" << data.width << ' ' << data.height << "\n255\n";
 		RTY_RAYTRACER_TRACE("Start process");
 
+		#define MT 0
+		#if MT
+		std::vector<int32_t> image_horizontal_iterator, image_vertical_iterator;
+		for (int32_t i = 0; i < data.width; ++i) { image_horizontal_iterator.push_back(i); }
+		for (int32_t i = data.height - 1; i >= 0; --i) { image_vertical_iterator.push_back( i); }
+		std::for_each(
+			std::execution::par, image_vertical_iterator.begin(),
+			image_vertical_iterator.end(), [&](int32_t y) {
+				RTY_PROFILE_SCOPE("Line");
+				RTY_RAYTRACER_TRACE("Scanlines remaining: {0}", y);
+				std::for_each(
+					std::execution::par, image_horizontal_iterator.begin(),
+					image_horizontal_iterator.end(), [&](int32_t x) {
+						RTY_PROFILE_SCOPE("Pixel");
+						Color3 pixel_color(0);
+						for (uint32_t sample = 0; sample < data.samples_per_pixel;
+							++sample) {
+							auto u = (float(x) + Random::RandomFloat()) / (data.width - 1);
+							auto v = (float(y) + Random::RandomFloat()) / (data.height - 1);
+							Ray ray = camera.ShootRay(u, v);
+							pixel_color += ComputeRayColor(ray, objects, data.max_depth);
+						}
+
+						WriteColor(output_stream, pixel_color, data.samples_per_pixel);
+					});
+			});
+		#else
 		// top to down
 		for (int32_t j = data.height - 1; j >= 0; --j) {
 			RTY_PROFILE_SCOPE("Line")
@@ -23,7 +51,7 @@ namespace raytracy {
 				RTY_PROFILE_SCOPE("Pixel")
 					Color3 pixel_color(0);
 				for (uint32_t sample = 0; sample < data.samples_per_pixel; ++sample) {
-						auto u = (float(i) + Random::RandomFloat()) / (data.width - 1);
+					auto u = (float(i) + Random::RandomFloat()) / (data.width - 1);
 					auto v = (float(j) + Random::RandomFloat()) / (data.height - 1);
 					Ray ray = camera.ShootRay(u, v);
 					pixel_color += ComputeRayColor(ray, objects, data.max_depth);
@@ -32,13 +60,15 @@ namespace raytracy {
 				WriteColor(output_stream, pixel_color, data.samples_per_pixel);
 			}
 		}
+		#endif
 		output_stream.close();
 
 		RTY_RAYTRACER_TRACE("Done.");
 	}
 
-	Color3 Raytracer::ComputeRayColor(const Ray& ray, const HitableCollection& objects, uint32_t depth) {
-
+	Color3 Raytracer::ComputeRayColor(const Ray& ray,
+		const HitableCollection& objects,
+		uint32_t depth) {
 		Hit hit;
 		if (depth <= 0) {
 			return Color3(0.0f);
@@ -59,8 +89,8 @@ namespace raytracy {
 			Color3(0.5f, 0.7f, 1.0f) * hit_value;
 	}
 
-	void Raytracer::WriteColor(std::ofstream& out, Color3 pixel_color, uint32_t samples_per_pixel) {
-
+	void Raytracer::WriteColor(std::ofstream& out, Color3 pixel_color,
+		uint32_t samples_per_pixel) {
 		auto scale = 1.0f / samples_per_pixel;
 		pixel_color = glm::sqrt(scale * pixel_color);
 		pixel_color = glm::clamp(pixel_color, 0.0f, 1.0f);
@@ -71,6 +101,4 @@ namespace raytracy {
 	}
 
 	void Raytracer::Shutdown() {}
-}
-
-
+}  // namespace raytracy
