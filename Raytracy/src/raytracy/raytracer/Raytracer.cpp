decac47b1ec3f5ad0b438ceb3761e25ee60d1cef
Wolfram Woerner <wolfram.woerner@outlook.de>
2023-01-21 13:16:05 +0100

    add iterative calculation

--- Raytracy/src/raytracy/raytracer/Raytracer.cpp
+++ Raytracy/src/raytracy/raytracer/Raytracer.cpp
@@ -44,7 +44,7 @@ namespace raytracy {
 						auto u = (float(x) + Random::RandomFloat()) / (image->width - 1);
 						auto v = (float(y) + Random::RandomFloat()) / (image->height - 1);
 						Ray ray = active_camera->ShootRay(u, v);
-						accumulated_color += ComputePixelColor(ray, *active_scene, image->max_depth); 
+						accumulated_color += ComputePixelColor(ray, *active_scene, image->max_depth);
 					}
 					accumulated_color /= image->samples_per_pixel;
 					accumulated_color = glm::sqrt(accumulated_color);
@@ -62,6 +62,8 @@ namespace raytracy {
 	glm::vec3 Raytracer::ComputePixelColor(const Ray& ray,
 		const Scene& objects,
 		uint32_t depth) {
+#define RECURSIVE 1
+#if RECURSIVE
 		Hit hit;
 		if (depth <= 0) {
 			return glm::vec3(0.0f);
@@ -82,6 +84,30 @@ namespace raytracy {
 		auto hit_value = 0.5f * (unit_direction.y + 1.0f);
 		return glm::vec3(1.0f) * (1.0f - hit_value) +
 			glm::vec3(0.5f, 0.7f, 1.0f) * hit_value;
+#else
+		Ray current_ray = ray;
+		glm::vec3 current_attenuation(1.0f);
+		for (uint32_t i = 0; i < depth; i++) {
+			Hit hit;
+			if (objects.HitObjects(current_ray, 0.001f, infinity, hit)) {
+				Ray scattered_ray;
+				glm::vec3 attenuation(0);
+				if (hit.material->Scatter(current_ray, hit, attenuation, scattered_ray)) {
+					current_attenuation *= attenuation;
+					current_ray = scattered_ray;
+				} else {
+					return glm::vec3(0);
+				}
+			} else {
+				glm::vec3 unit_direction = glm::normalize(current_ray.GetDirection());
+				auto hit_value = 0.5f * (unit_direction.y + 1.0f);
+				glm::vec3 color = glm::vec3(1.0f) * (1.0f - hit_value) +
+					glm::vec3(0.5f, 0.7f, 1.0f) * hit_value;
+				return current_attenuation * color;
+			}
+		}
+		return glm::vec3(0);
+#endif
 	}
 
 	void Raytracer::WriteImage() {
@@ -92,9 +118,9 @@ namespace raytracy {
 		output_stream << "P3\n" << image->width << ' ' << image->height << "\n255\n";
 		RTY_RAYTRACER_TRACE("Save as PPM...");
 
-		for (int32_t j = (int32_t) image->height - 1; j >= 0; j--) {
+		for (int32_t j = (int32_t)image->height - 1; j >= 0; j--) {
 			// left to right
-			for (int32_t i = 0; i < (int32_t) image->width; i++) {
+			for (int32_t i = 0; i < (int32_t)image->width; i++) {
 				glm::vec3 pixel_color = accumulated_color_data[i + j * image->width];
 
 				output_stream << static_cast<int>(255.0f * pixel_color.r) << ' '
