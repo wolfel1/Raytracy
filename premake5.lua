bf477d2be8427c27ea23d629b84648e0516ac495
Wolfram Woerner <wolfram.woerner@outlook.de>
2024-04-13 15:59:07 +0200

    Add test project

--- premake5.lua
+++ premake5.lua
@@ -7,6 +7,7 @@ workspace "Raytracy"
 		"Release"
 	}
 
+
 	flags {
 		"MultiProcessorCompile"
 	}
@@ -138,3 +139,41 @@ project "Sandbox"
 		defines "RTY_RELEASE"
 		runtime "Release"
 		optimize "on"
+
+project "Test"
+	location "Test"
+	kind "ConsoleApp"
+	language "C++"
+	cppdialect "C++20"
+	staticruntime "off"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files {
+		"%{prj.name}/**.h",
+		"%{prj.name}/**.cpp"
+	}
+
+	includedirs {
+		"Raytracy/src",
+		"%{IncludeDir.glm}"
+	}
+
+	defines {
+		"_CRT_SECURE_NO_WARNINGS",
+		"_SILENCE_ALL_MS_EXT_DEPRECATION_WARNINGS"
+	}
+
+	links {
+		"Raytracy"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+
+	
+	filter "configurations:Debug"
+		defines "RTY_DEBUG"
+		runtime "Debug"
+		symbols "on"
